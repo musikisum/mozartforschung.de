e80a466d25b2362e3c6504b4d8d00c0f8fef78dc
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2026-03-29 19:04:27 +0200

    Add web.config, .htaccess and fonts to Eleventy passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/downloads");
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/fonts");
+  eleventyConfig.addPassthroughCopy({ "src/web.config": "web.config" });
+  eleventyConfig.addPassthroughCopy({ "src/.htaccess": ".htaccess" });
 
   return {
     dir: {
